6cb049ad3d77fa779854424d3c634c3b1bbde85f
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2016-04-13 08:18:56 +0100

    corrections

--- helpers/tex2md.sh
+++ helpers/tex2md.sh
@@ -10,7 +10,8 @@ sed 's/\\thai[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
 sed 's/\\speaker[{]\([^}]\+\)[}]/\\emph{\1}/g' |\
 sed 's/\\begin[{]packeditemize[}]/\\begin{itemize}/g' |\
 sed 's/\\end[{]packeditemize[}]/\\end{itemize}/g' |\
-sed 's/\\linebreak *//g' |\
+sed 's/\\linebreak//g' |\
+sed 's/\\newline//g' |\
 sed 's/\\LaTeX/LaTeX/g' |\
 pandoc --smart --normalize --from=latex --to=markdown |\
 sed 's/\([^\\]\)\\$/\1\\\\/' > "$DEST"
